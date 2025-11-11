e2f40e2984c18a143c53242d33205abf6abffaef
german-spb <german@pushkin-spb.com>
2025-11-11 16:20:17 +0300

    first commit

---
+++ tsg4/forms.py
@@ -0,0 +1,39 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class RegistrationForm(UserCreationForm):
+    username = forms.CharField(
+        max_length=150,
+        label='Имя пользователя',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Введите имя пользователя'
+        })
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Введите email'
+        })
+    )
+    password1 = forms.CharField(
+        max_length=128,
+        label='Пароль',
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Введите пароль'
+        })
+    )
+    password2 = forms.CharField(
+        max_length=128,
+        label='Подтверждение пароля',
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Повторите пароль'
+        })
+    )
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2', ]
---
+++ tsg4/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from forms import RegistrationForm
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.contrib.auth.models import User
+
+class CustomRegistrationView(CreateView):
+    form_class = RegistrationForm
+    template_name = 'signup.html'
+    extra_context = {'title': 'Регистрация на сайте'}
+
+    def get_success_url(self):
+        return reverse_lazy('tsg4:login')
+
+    def form_valid(self, form):
+        user = form.save()
+        return super().form_valid(form)
