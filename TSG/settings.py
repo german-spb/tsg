c96c81684cea029d19e5b1e1a865867648409617
german-spb <german@pushkin-spb.com>
2025-11-24 17:15:51 +0300

    insert table

--- TSG/settings.py
+++ TSG/settings.py
@@ -134,3 +134,6 @@ LOGOUT_REDIRECT_URL = '/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- tsg4/models.py
+++ tsg4/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Documents(models.Model):
+    title = models.CharField(max_length=300)
+    document = models.FileField(upload_to='documents/')
+
+    def __str__(self):
+        return self.title
+
